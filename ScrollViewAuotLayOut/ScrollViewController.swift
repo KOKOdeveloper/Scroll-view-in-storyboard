c68c37bf70eea408d99abf98b875c0a7ea73d7ca
Nikola Rusev <nikola.rusev@yahoo.co.uk>
2018-05-12 22:58:19 +0200

    initial commit implemented scrollview in storyboard with content size depends of height and width of objects with constraints

---
+++ ScrollViewAuotLayOut/ScrollViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ScrollViewController.swift
+//  ScrollViewAuotLayOut
+//
+//  Created by NIKOLA RUSEV on 5/7/18.
+//  Copyright © 2018 NIKOLA RUSEV. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textLabel.text = "Kobe Bryant would do anything necessary to stop LeBron James, including what he believes is the obvious. The Los Angeles Lakers legend said Sunday on FOX Sports’ “Chris and Caron” radio show he’d try to contain the Cleveland Cavaliers superstar by stopping him from driving to his left. Bryant made the observation after recognizing some of James’ biggest shots — including his buzzer-beater Saturday against the Toronto Raptors and his game-winner in the first round against the Indiana Pacers — come after he goes in that direction. “For one thing, you have to stop letting him go left,” Bryant said. “Every big shot that he makes is going left. He’s remarkable about getting to that left hand and being able to raise up and shoot. That’s the first thing I would do, disrupt his ability to go left. “It’s amazing, all those shots are either coming right shoulder from the post or off the dribble with his left hand. That’s the first thing I would look at if I was guarding him in these playoffs— make him do something different besides going to that left.” James is averaging 34.8 points, 9.5 rebounds, and 8.8 assists over 10 playoff games this season. The Pacers failed to stop him in their first-round series against the Cavs, and the Raptors, down 3-0 in their Eastern Conference semifinal series, look destined for a similar fate. Whether James’ next playoff opponent — likely the Boston Celtics — heeds Bryant’s advice remains to be seen, but Kobe’s tip seems as good a starting point as any. Kobe Bryant would do anything necessary to stop LeBron James, including what he believes is the obvious. The Los Angeles Lakers legend said Sunday on FOX Sports’ “Chris and Caron” show he’d try to contain the Cleveland Cavaliers superstar by stopping him from driving to his left. Bryant made the observation after recognizing some of James’ biggest shots come after he goes in that direction. “For one thing, you have to stop letting him go left,” Bryant said. “Every big shot that he makes is going left. He’s remarkable about getting to that left hand and being able to raise up and shoot. That’s the first thing I would do, disrupt his ability to go left. It’s amazing, all those shots are either Kobe Bryant would do anything necessary to stop LeBron James, including what he believes is the obvious. The Los Angeles Lakers legend said Sunday on FOX Sports’ “Chris and Caron” show he’d try to contain the Cleveland Cavaliers superstar by stopping him from driving to his left. Bryant made the observation after recognizing some of James’ biggest shots come after he goes in that direction. “For one thing, you have to stop letting him go left,” Bryant said. “Every big shot that he makes is going left. He’s remarkable about getting to that left hand and being able to raise up and shoot. That’s the first thing I would do, disrupt his ability to go left. It’s amazing, all those shots are eitherKobe Bryant would do anything necessary to stop LeBron James, including what he believes is the obvious. The Los Angeles Lakers legend said Sunday on FOX Sports’ “Chris and Caron” show he’d try to contain the Cleveland Cavaliers superstar by stopping him from driving to his left. Bryant made the observation after recognizing some of James’ biggest shots come after he goes in that direction. “For one thing, you have to stop letting him go left,” Bryant said. “Every big shot that he makes is going left. He’s remarkable about getting to that left hand and being able to raise up and shoot. That’s the first thing I would do, disrupt his ability to go left. It’s amazing, all those shots are eitherKobe Bryant would do anything necessary to stop LeBron James, including what he believes is the obvious. The Los Angeles Lakers legend said Sunday on FOX Sports’ “Chris and Caron” show he’d try to contain the Cleveland Cavaliers superstar by stopping him from driving to his left. Bryant made the observation after recognizing some of James’ biggest shots come after he goes in that direction. “For one thing, you have to stop letting him go left,” Bryant said. “Every big shot that he makes is going left. He’s remarkable about getting to that left hand and being able to raise up and shoot. That’s the first thing I would do, disrupt his ability to go left. It’s amazing, all those shots are either koko"
+        
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
